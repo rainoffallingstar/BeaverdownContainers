3080e9a61468d1fc714d59be48f632d47c2e3f60
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-09-11 11:01:12 +0800

    fix samtools error

--- beaverstudioUpdate.R
+++ beaverstudioUpdate.R
@@ -1,7 +1,7 @@
 dockerR::container_update(containerid = "beaverstudio",
                           imageid = "fallingstar10/beaverstudio:latest",
                           force_rm_container = FALSE,
-                          volume = "-v /d/beaver:/home/builduser",
+                          volume = "-v /d/beaver:/home/builduser -v /d/fonts-main:/etc/rstudio/fonts",
                           ports = "-p 8765:8787 -p 8675:8080",
                           ifD = TRUE,
                           restart = TRUE,
